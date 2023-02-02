658a3ed6f60b76f39f0fb5cbeaba22a1bd6e6202
Dharvo <adedave77@gmail.com>
2023-02-02 19:03:58 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace ConsoleToWeb { 
+    class Program{ 
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World !");
+            CreateHostBuilder(args).Build().Run();
+            //Console.ReadLine();
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).
+            ConfigureWebHostDefaults(webHost =>
+            {
+                webHost.UseStartup<Startup>();
+            });
+            }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ConsoleToWeb
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services){}
+        public void Configure(IApplicationBuilder app) {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                    {
+                        await context.Response.WriteAsync("Hello this is a Test from Dharvo's Inc. VybzTech");
+                    });
+                endpoints.MapGet("/test", async context =>
+                    {
+                        await context.Response.WriteAsync("Hello this is a TestPage from Test's Inc. VybzTech");
+                    });
+            });
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Hello this is a Test from Use - I \n VybzTech \n");
+                await next();
+                await context.Response.WriteAsync("Hello this is a Test from Use - II \n VybzTech \n");
+                await next();
+                await context.Response.WriteAsync("Hello this is a Test from Use II - I \n VybzTech \n");
+                await next();
+                await context.Response.WriteAsync("Hello this is a Test from Use II - II \n VybzTech \n");
+                app.Run(async context =>
+                {
+                    await context.Response.WriteAsync("Hello this is a Run Test from VybzTech. \n");
+                });
+            });
+        }
+    }
+}
